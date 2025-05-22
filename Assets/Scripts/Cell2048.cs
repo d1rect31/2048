115540573500d98d00060247c73b338ca5801aa7
d1rect31 <altan.ay204@gmail.com>
2025-05-23 02:00:41 +0500

    AI refactoring shit
    refactoring through copilot

--- Assets/Scripts/Cell2048.cs
+++ Assets/Scripts/Cell2048.cs
@@ -8,6 +8,7 @@ public class Cell2048 : MonoBehaviour
     public Cell2048 up;
     public Cell2048 down;
     public Fill2048 fill;
+    public bool mergedThisTurn = false;
     public void OnEnable()
     {
         GameController.slide += OnSlide;
@@ -24,70 +25,209 @@ public class Cell2048 : MonoBehaviour
         {
             if (up != null)
                 return;
-            Cell2048 cell = this;
-            SlideUp(cell);
+            SlideUp(this);
         }
         if (recievedDirection == "down")
         {
-
+            if (down != null)
+                return;
+            SlideDown(this);
         }
         if (recievedDirection == "left")
         {
-
+            if (left != null)
+                return;
+            SlideLeft(this);
         }
         if (recievedDirection == "right")
         {
-
+            if (right != null)
+                return;
+            SlideRight(this);
         }
     }
     void SlideUp(Cell2048 currentCell)
     {
-        Debug.Log(currentCell.name);
-        if (currentCell.down == null)
-            return; 
-        if (currentCell.fill != null)
-        {
-            Cell2048 nextCell = currentCell.down;
-            while (nextCell.down != null && nextCell.fill == null)
-            {
-                nextCell = nextCell.down;
-            }
-            if (nextCell.fill != null)
-            {
-                if (currentCell.fill.value == nextCell.fill.value)
-                {
-                    Debug.Log("Merging");
-                    nextCell.fill.transform.parent = currentCell.transform;
-                    currentCell.fill = nextCell.fill;
-                    nextCell.fill = null;
-                }
-                else
-                {
-                    Debug.Log("Not Merged");
-                    nextCell.fill.transform.parent = currentCell.down.transform;
-                    currentCell.down.fill = nextCell.fill;
-                    nextCell.fill = null;
-                }
-            }
-        }
-        else
-        {
-            Cell2048 nextCell = currentCell.down;
-            while (nextCell.down != null && nextCell.fill == null)
-            {
-                nextCell = nextCell.down;
-            }
-            if (nextCell.fill != null)
-            {
-                nextCell.fill.transform.parent = currentCell.transform;
-                currentCell.fill = nextCell.fill;
-                nextCell.fill = null;
-                SlideUp(currentCell);
-                Debug.Log("Slided to Empty");
-            }
-        }
         if (currentCell.down == null)
             return;
-        SlideUp(currentCell.down);
+
+        Cell2048 nextCell = FindNextFilledDown(currentCell);
+        if (nextCell == null || nextCell.fill == null)
+        {
+            if (currentCell.down != null)
+                SlideUp(currentCell.down);
+            return;
+        }
+
+        if (currentCell.fill == null)
+        {
+            nextCell.fill.transform.parent = currentCell.transform;
+            currentCell.fill = nextCell.fill;
+            nextCell.fill = null;
+            SlideUp(currentCell);
+        }
+        else if (currentCell.fill.value == nextCell.fill.value && !currentCell.mergedThisTurn && !nextCell.mergedThisTurn)
+        {
+            currentCell.fill.Double();
+            nextCell.fill = null;
+            currentCell.mergedThisTurn = true;
+        }
+        else if (currentCell.down.fill == null)
+        {
+            nextCell.fill.transform.parent = currentCell.down.transform;
+            currentCell.down.fill = nextCell.fill;
+            nextCell.fill = null;
+        }
+
+        if (currentCell.down != null)
+            SlideUp(currentCell.down);
+    }
+    void SlideDown(Cell2048 currentCell)
+    {
+        if (currentCell.up == null)
+            return;
+
+        Cell2048 nextCell = FindNextFilledUp(currentCell);
+        if (nextCell == null || nextCell.fill == null)
+        {
+            if (currentCell.up != null)
+                SlideDown(currentCell.up);
+            return;
+        }
+
+        if (currentCell.fill == null)
+        {
+            nextCell.fill.transform.parent = currentCell.transform;
+            currentCell.fill = nextCell.fill;
+            nextCell.fill = null;
+            SlideDown(currentCell);
+        }
+        else if (currentCell.fill.value == nextCell.fill.value && !currentCell.mergedThisTurn && !nextCell.mergedThisTurn)
+        {
+            currentCell.fill.Double();
+            nextCell.fill = null;
+            currentCell.mergedThisTurn = true;
+        }
+        else if (currentCell.up.fill == null)
+        {
+            nextCell.fill.transform.parent = currentCell.up.transform;
+            currentCell.up.fill = nextCell.fill;
+            nextCell.fill = null;
+        }
+
+        if (currentCell.up != null)
+            SlideDown(currentCell.up);
+    }
+
+    void SlideLeft(Cell2048 currentCell)
+    {
+        if (currentCell.right == null)
+            return;
+
+        Cell2048 nextCell = FindNextFilledRight(currentCell);
+        if (nextCell == null || nextCell.fill == null)
+        {
+            if (currentCell.right != null)
+                SlideLeft(currentCell.right);
+            return;
+        }
+
+        if (currentCell.fill == null)
+        {
+            nextCell.fill.transform.parent = currentCell.transform;
+            currentCell.fill = nextCell.fill;
+            nextCell.fill = null;
+            SlideLeft(currentCell);
+        }
+        else if (currentCell.fill.value == nextCell.fill.value && !currentCell.mergedThisTurn && !nextCell.mergedThisTurn)
+        {
+            currentCell.fill.Double();
+            nextCell.fill = null;
+            currentCell.mergedThisTurn = true;
+        }
+        else if (currentCell.right.fill == null)
+        {
+            nextCell.fill.transform.parent = currentCell.right.transform;
+            currentCell.right.fill = nextCell.fill;
+            nextCell.fill = null;
+        }
+
+        if (currentCell.right != null)
+            SlideLeft(currentCell.right);
+    }
+
+    void SlideRight(Cell2048 currentCell)
+    {
+        if (currentCell.left == null)
+            return;
+
+        Cell2048 nextCell = FindNextFilledLeft(currentCell);
+        if (nextCell == null || nextCell.fill == null)
+        {
+            if (currentCell.left != null)
+                SlideRight(currentCell.left);
+            return;
+        }
+
+        if (currentCell.fill == null)
+        {
+            nextCell.fill.transform.parent = currentCell.transform;
+            currentCell.fill = nextCell.fill;
+            nextCell.fill = null;
+            SlideRight(currentCell);
+        }
+        else if (currentCell.fill.value == nextCell.fill.value && !currentCell.mergedThisTurn && !nextCell.mergedThisTurn)
+        {
+            currentCell.fill.Double();
+            nextCell.fill = null;
+            currentCell.mergedThisTurn = true;
+        }
+        else if (currentCell.left.fill == null)
+        {
+            nextCell.fill.transform.parent = currentCell.left.transform;
+            currentCell.left.fill = nextCell.fill;
+            nextCell.fill = null;
+        }
+
+        if (currentCell.left != null)
+            SlideRight(currentCell.left);
+    }
+    private Cell2048 FindNextFilledDown(Cell2048 cell)
+    {
+        Cell2048 next = cell.down;
+        while (next != null && next.fill == null && next.down != null)
+        {
+            next = next.down;
+        }
+        return next;
+    }
+    private Cell2048 FindNextFilledUp(Cell2048 cell)
+    {
+        Cell2048 next = cell.up;
+        while (next != null && next.fill == null && next.up != null)
+        {
+            next = next.up;
+        }
+        return next;
+    }
+
+    private Cell2048 FindNextFilledRight(Cell2048 cell)
+    {
+        Cell2048 next = cell.right;
+        while (next != null && next.fill == null && next.right != null)
+        {
+            next = next.right;
+        }
+        return next;
+    }
+
+    private Cell2048 FindNextFilledLeft(Cell2048 cell)
+    {
+        Cell2048 next = cell.left;
+        while (next != null && next.fill == null && next.left != null)
+        {
+            next = next.left;
+        }
+        return next;
     }
 }
--- Assets/Scripts/Fill2048.cs
+++ Assets/Scripts/Fill2048.cs
@@ -5,10 +5,35 @@ using UnityEngine.UI;
 public class Fill2048 : MonoBehaviour
 {
     public int value;
+    private float speed = 5000f;
     [SerializeField] GameObject valueDisplay;
+    public bool mergedThisTurn;
     public void FillValueUpdate(int newValue)
     {
         value = newValue;
+        if (valueDisplay != null)
+        {
+            valueDisplay.GetComponent<TextMeshProUGUI>().text = value.ToString();
+        }
+    }
+    private void Update()
+    {
+        if (transform.localPosition != Vector3.zero)
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, Vector3.zero, Time.deltaTime * speed);
+        }
+        else if (mergedThisTurn == false)
+        {
+            if (transform.parent.GetChild(0) != this.transform)
+            {
+                Destroy(transform.gameObject);
+            }
+            mergedThisTurn = true;
+        }
+    }
+    public void Double()
+    {
+        value *= 2;
         if (valueDisplay != null)
         {
             valueDisplay.GetComponent<TextMeshProUGUI>().text = value.ToString();
